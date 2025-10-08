7d2483e10b6460a57945a506a9265060e716906d
Vasco <zanchivasco@gmail.com>
2025-10-08 14:21:07 -0300

    update field

--- field/BicWin2025/summary_geophone_lines_2025/load_inversion_results.py
+++ field/BicWin2025/summary_geophone_lines_2025/load_inversion_results.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import csv
 
 #%%
 
@@ -108,7 +109,7 @@ for i in range(len(dict_results['inverted_data'])):
 
     dict_results[dateacq] = open_inverted_data(pkl_file_path)
 
-#%% choisir ce qui nous interesse (Young? Epaisseur? ...) et ca donne la valeur max de la kde
+#%% choisir ce qui nous interesse (Epaisseur? Young? ...) et ca donne la valeur max de la kde
 
 idx_datatoplot = 1 # thickness : 0 ; Young: 1 ; Poisson : 2 ; rho : 3
 
@@ -118,6 +119,59 @@ list_maxvalkde =[]
 argmaxkde = find_max_kde(dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['values'])
 maxvalkde = dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['values'][argmaxkde]
 
+# trouver le maxkde (meilleure estimation) pour chaque param de glace
+
+dict_results[dateacq]
+
+for i in range(len(dict_results['inverted_data'])):
+    dateacq = dict_results['inverted_data'][i]
+    date = inverted_dates[i]
+    for idx_datatoplot in [0,1,2,3]:
+        argmaxkde = find_max_kde(dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['values'])
+        maxvalkde = dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['values'][argmaxkde]
+        dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['argmaxkde'] = argmaxkde
+        dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['maxvalkde'] = maxvalkde
+
+# %% create csv file with all information (including coordinates and water depths)
+coord_water_level_filepath = f"{disk}/General/Summary_geophone_lines/water_level_acquisitions/coord_waterlevel_allacq.csv"
+
+with open(coord_water_level_filepath, mode ='r')as file:
+    csvFile = csv.reader(file, delimiter=',')
+    count=0
+    data = []
+    for lines in csvFile:
+        if count==0:
+            header = lines
+        else:
+            data.append(lines)
+        count+=1
+print(header)
+
+data = np.array(data)
 
 
+data_new = np.empty((data.shape[0], data.shape[1]+4),dtype=object)
+data_new[:data.shape[0],:data.shape[1]] = data
+header_new = header + ['thickness (m)', 'Young modulus (Pa)', 'Poisson ratio', 'density (kg/m^3)']
+
+for i in range(len(dict_results['inverted_data'])):
+    dateacq = dict_results['inverted_data'][i]
+    date = inverted_dates[i]
+    completedatestr = f'{year}{date}'
+    acqnum_str = str(inverted_acqs[i])
+    idx_row = np.where((data[:,0]==completedatestr)&(data[:,1]==acqnum_str))[0][0]
+    print(idx_row)
+    for idx_datatoplot in [0,1,2,3]:
+        data_new[idx_row,data.shape[1]+idx_datatoplot] = dict_results[dateacq]['data_to_plot_'+str(idx_datatoplot)]['maxvalkde']
+
+csvpath2save = f"{disk}/General/Summary_geophone_lines/results_acquisitions.csv"
+
+with open(csvpath2save, 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f,delimiter=',')
+
+    # write the header
+    writer.writerow(header_new)
+
+    # write multiple rows
+    writer.writerows(data_new)
 # %%
